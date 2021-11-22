af01d1503edefd67b589cbb95d74f58a6a057dea
Tim Triche, Jr <tim.triche@gmail.com>
2021-11-22 12:44:08 -0500

    initial push

---
+++ R/TET2.R
@@ -0,0 +1,14 @@
+#' Rerun the TET2 contrast from Figueroe et al. 2010. 
+#' 
+#' @param   eset    an ExpressionSet of HELP data
+#'
+#' @return          several contrasts
+#' 
+#' @import          limma
+#' 
+#' @export
+TET2 <- function(eset) {
+
+
+
+}
